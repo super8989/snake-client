5f543dc247fa5e60ec42bd1dad9f8750ecaffe6f
Sam Yang <samyang8989@gmail.com>
2020-07-29 19:36:05 +0000

    added setupInput and hanldeUserInput to listen to user inputs

--- play.js
+++ play.js
@@ -1,4 +1,26 @@
 const { connect } = require('./client');
 
+const setupInput = () => {
+	const stdin = process.stdin;
+
+	stdin.setRawMode(true);
+	stdin.setEncoding('utf8');
+	stdin.resume();
+
+	stdin.on('data', (key) => {
+		handleUserInput(key);
+	});
+
+	return stdin;
+};
+
+handleUserInput = (key) => {
+	if (key === '\u0003') {
+		process.exit();
+	}
+};
+
+setupInput();
+
 console.log('Connecting ...');
 connect();
