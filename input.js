b08dbb2b38969b19d01f666f710fbc7b25cdbe4c
Sam Yang <samyang8989@gmail.com>
2020-07-29 19:42:50 +0000

    moved setupInput logic to input.js

---
+++ input.js
@@ -0,0 +1,21 @@
+const setupInput = () => {
+	const stdin = process.stdin;
+
+	stdin.setRawMode(true);
+	stdin.setEncoding('utf8');
+	stdin.resume();
+
+	stdin.on('data', (key) => {
+		handleUserInput(key);
+	});
+
+	return stdin;
+};
+
+const handleUserInput = (key) => {
+	if (key === '\u0003') {
+		process.exit();
+	}
+};
+
+module.exports = { setupInput };
--- play.js
+++ play.js
@@ -1,26 +1,7 @@
 const { connect } = require('./client');
-
-const setupInput = () => {
-	const stdin = process.stdin;
-
-	stdin.setRawMode(true);
-	stdin.setEncoding('utf8');
-	stdin.resume();
-
-	stdin.on('data', (key) => {
-		handleUserInput(key);
-	});
-
-	return stdin;
-};
-
-handleUserInput = (key) => {
-	if (key === '\u0003') {
-		process.exit();
-	}
-};
-
-setupInput();
+const { setupInput } = require('./input');
 
 console.log('Connecting ...');
 connect();
+
+setupInput();
