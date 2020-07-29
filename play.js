52c865d65dcf6d7ee57ebec24cbdec19fdc17d29
Sam Yang <samyang8989@gmail.com>
2020-07-29 18:08:24 +0000

    handle incoming data and console log it for the player

--- play.js
+++ play.js
@@ -11,6 +11,10 @@ const connect = function () {
 	// interpret incoming data as text
 	conn.setEncoding('utf8');
 
+	conn.on('data', (data) => {
+		console.log('Server says: ', data);
+	});
+
 	return conn;
 };
 
