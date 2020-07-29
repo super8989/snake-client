4faeec8c7a8c40825a8c092a291808007582e598
Sam Yang <samyang8989@gmail.com>
2020-07-29 20:04:15 +0000

    input module accepts connection object returned from connect()

--- input.js
+++ input.js
@@ -1,4 +1,8 @@
-const setupInput = () => {
+// Stores the active TCP connection object.
+let connection;
+
+const setupInput = (conn) => {
+	connection = conn;
 	const stdin = process.stdin;
 
 	stdin.setRawMode(true);
@@ -15,6 +19,22 @@ const setupInput = () => {
 const handleUserInput = (key) => {
 	if (key === '\u0003') {
 		process.exit();
+	}
+
+	if (key === 'w') {
+		console.log('up');
+	}
+
+	if (key === 'a') {
+		console.log('left');
+	}
+
+	if (key === 's') {
+		console.log('down');
+	}
+
+	if (key === 'd') {
+		console.log('right');
 	}
 };
 
