673b8b44e3b41e05761d9cc993c8344abf7438e6
Sam Yang <samyang8989@gmail.com>
2020-07-29 23:08:35 +0000

    created constants for variables

---
+++ constants.js
@@ -0,0 +1,15 @@
+const IP = '135.23.222.131';
+const PORT = 50541;
+const MOVE_UP_KEY = 'w';
+const MOVE_DOWN_KEY = 's';
+const MOVE_RIGHT_KEY = 'd';
+const MOVE_LEFT_KEY = 'a';
+
+module.exports = {
+	IP,
+	PORT,
+	MOVE_UP_KEY,
+	MOVE_DOWN_KEY,
+	MOVE_RIGHT_KEY,
+	MOVE_LEFT_KEY,
+};
